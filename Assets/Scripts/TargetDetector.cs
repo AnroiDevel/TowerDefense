aac6303f8e2b6274b153defbfbff274f2e26fce1
AnroiDevel <AnroiDevel@gmail.com>
2021-10-18 14:04:27 +0300

    Created scripts "Dragable" and "TargetDetector".

---
+++ Assets/Scripts/TargetDetector.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TowerDefese
+{
+    public class TargetDetector : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var target = collision.gameObject.GetComponent<SpriteRenderer>();
+            if (target != null)
+                target.color = Color.red;
+
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            var target = collision.gameObject.GetComponent<SpriteRenderer>();
+            if (target != null)
+                target.color = Color.white;
+
+        }
+
+    }
+
+}
