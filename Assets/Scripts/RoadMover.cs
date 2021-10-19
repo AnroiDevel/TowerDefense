4d1d5ab3da07f91daca60751327505e1ffddb6d5
AnroiDevel <AnroiDevel@gmail.com>
2021-10-19 14:02:11 +0300

    made an experimental way to move along the path

---
+++ Assets/Scripts/RoadMover.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.U2D;
+
+
+namespace TowerDefese
+{
+    public class RoadMover : MonoBehaviour
+    {
+        [SerializeField] private SpriteShapeController _spriteShapeController;
+        private List<Vector2> _points;
+        [SerializeField] private Rigidbody2D _body2D;
+
+        private void Start()
+        {
+            GetWayPoints();
+        }
+
+        private void GetWayPoints()
+        {
+            _points = new List<Vector2>();
+            var way = _spriteShapeController.spline;
+            var count = way.GetPointCount();
+            for (int i = 0; i < count; i++)
+            {
+                _points.Add(way.GetPosition(i));
+            }
+
+            StartCoroutine(Walk());
+        }
+
+        private IEnumerator Walk()
+        {
+            var i = 0;
+            while (i < _points.Count)
+            {
+                transform.localPosition = Vector2.MoveTowards(transform.localPosition, _points[i], 0.01f);
+                yield return new WaitForSeconds(0.01f);
+                if ((Vector2)transform.localPosition == _points[i])
+                    i++;
+
+            }
+
+        }
+    }
+
+}
