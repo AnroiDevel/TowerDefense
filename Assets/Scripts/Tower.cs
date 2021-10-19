5e140f3a53ff1d2476b420a8df4b24f0f50607a2
AnroiDevel <AnroiDevel@gmail.com>
2021-10-19 14:02:11 +0300

    made an experimental way to move along the path

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -8,14 +8,19 @@ namespace TowerDefese
     public class Tower : MonoBehaviour
     {
         [SerializeField] private TargetDetector _targetDetector;
+        [SerializeField] private float _rotationSpeed;
 
         private void Update()
         {
             if (_targetDetector.Target != null)
             {
                 var target = _targetDetector.Target;
-                var angle = Vector2.Angle(Vector2.up, target.position - transform.position);
-                transform.eulerAngles = new Vector3(0f, 0f, transform.position.x < target.position.x ? -angle : angle);
+
+                //var angle = Vector2.Angle(Vector2.up, target.position - transform.position);
+                //transform.eulerAngles = new Vector3(0f, 0f, transform.position.x < target.position.x ? -angle : angle);  //Áûסענûי ןמגמנמע
+
+                Vector2 dir = target.position - transform.position;
+                transform.up = Vector2.MoveTowards(transform.up, dir, Time.deltaTime * _rotationSpeed);
             }
 
         }
