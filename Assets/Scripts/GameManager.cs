a7100ba6941f85703f4e32c0c002c5504c87b609
AnroiDevel <AnroiDevel@gmail.com>
2021-10-20 17:23:11 +0300

    Add Button for Enemy

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TowerDefese;
+using UnityEngine.Animations;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject _skeletonPrefab;
+    [SerializeField] private Transform _startTransform;
+
+    [SerializeField] private GameObject _nameUIPrefab;
+    private Text _name;
+    private ParentConstraint _parentConstraint;
+    private Skeleton _enemy;
+
+    public void CreateEnemy()
     {
-        
+        var enemyGO = Instantiate(_skeletonPrefab);
+        enemyGO.transform.position = _startTransform.position;
+        _enemy = enemyGO.GetComponent<Skeleton>();
+
+        var nameGO = Instantiate(_nameUIPrefab);
+
+        _name = nameGO.GetComponent<Text>();
+        _parentConstraint = nameGO.GetComponent<ParentConstraint>();
+
+        _name.text = _enemy.name;
+
+        _parentConstraint.AddSource(new ConstraintSource());
+        var soursetransform = _parentConstraint.GetSource(0);
+        soursetransform.sourceTransform = _enemy.transform;
+
+        _parentConstraint.translationAtRest = Vector3.zero;
+        _parentConstraint.constraintActive = true;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
