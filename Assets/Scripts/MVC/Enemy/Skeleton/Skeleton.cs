68bbe1781e19feacb4fea830e04b9dcc1ee67033
AnroiDevel <AnroiDevel@gmail.com>
2021-10-23 13:57:48 +0300

    test enemy maker created

--- Assets/Scripts/MVC/Enemy/Skeleton/Skeleton.cs
+++ Assets/Scripts/MVC/Enemy/Skeleton/Skeleton.cs
@@ -1,19 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 
 namespace TowerDefese
 {
-    public class Skeleton : EnemyView<SkeletonModel,EnemyController<SkeletonModel>>
+    public class Skeleton : EnemyView<SkeletonModel, EnemyController<SkeletonModel>>
     {
         [SerializeField] private SpawnManagerScriptableObject _sceletonSO;
 
-        private void Start()
+        public override void Awake()
         {
+            base.Awake();
             Model.Name = _sceletonSO.Name;
             Model.Armor = _sceletonSO.Armor;
             Model.Speed = _sceletonSO.Speed;
+
         }
     }
 }
--- Assets/Scripts/MVC/Enemy/Skeleton/SkeletonModel.cs
+++ Assets/Scripts/MVC/Enemy/Skeleton/SkeletonModel.cs
@@ -3,17 +3,6 @@
     [System.Serializable]
     public class SkeletonModel : EnemyModel
     {
-        private readonly string _name = "Скелет";
-        private readonly float _skeletonSpeed = 1.0f;
-        private readonly Armor _armor = Armor.Medium;
-
-
-        public SkeletonModel()
-        {
-            Speed = _skeletonSpeed;
-            Armor = _armor;
-            Name = _name;
-        }
 
     }
 }
